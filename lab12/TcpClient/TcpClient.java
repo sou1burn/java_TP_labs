28f96e205596d9481ef7d761fb7af4dac8bdb2aa
sou1burn <koryakinv_04@mail.ru>
2025-06-02 18:33:07 +0300

    fix client

--- lab12/TcpClient/TcpClient.java
+++ lab12/TcpClient/TcpClient.java
@@ -9,41 +9,49 @@ public class TcpClient {
 
     public static void main(String[] args) {
         try (Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
-             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
             
-            String welcome = in.readLine();
-            System.out.print(welcome);
+                String welcome = in.readLine();
+                System.out.print(welcome);
 
-            String usrName = input.readLine();
-            out.write(usrName + "\n");
-            out.flush();
-            
-            new Thread(() -> {
+                String usrName = input.readLine();
+                out.write(usrName + "\n");
+                out.flush();
+                
+                new Thread(() -> {
+                    try {
+                        String msg;
+                        while ((msg = in.readLine()) != null) {
+                            System.out.println(msg);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Connection closed.");
+                    } finally {
+                        try {
+                            System.in.close(); 
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
                 try {
-                    String msg;
-                    while ((msg = in.readLine()) != null) {
-                        System.out.println(msg);
+                    String userInput;
+                    while ((userInput = input.readLine()) != null) {
+                        if ("/exit".equalsIgnoreCase(userInput)) {
+                            socket.close();
+                            break;
+                        }
+                        out.write(userInput + "\n");
+                        out.flush();
                     }
                 } catch (IOException e) {
-                    System.out.println("Connection closed.");
-                } finally {
-                    try {
-                        System.in.close(); 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-
-            String userInput;
-            while ((userInput = input.readLine()) != null) {
-                out.write(userInput + "\n");
-                out.flush();
+                    System.out.println("Connection closed because of server exited.");
+                }   
+            } catch (IOException e) {
+                System.err.println("Could not connect to server.");
             }
-        } catch (IOException e) {
-            System.err.println("Could not connect to server.");
-        }
     }
 }
