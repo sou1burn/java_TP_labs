c8ed7350b71264e0a700f71d32e9b44bf0db0392
sou1burn <koryakinv_04@mail.ru>
2025-06-09 01:45:27 +0300

    chut-chut

--- ekzamen/Ekzamen.java
+++ ekzamen/Ekzamen.java
@@ -1,3 +1,9 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
 
 
 public class Ekzamen {
@@ -67,7 +73,33 @@ public class Ekzamen {
         return totalSum;
     }
 
+    // public static int multiThreadedMinimumOfArray(int[] arr, int threadCount) {
+    //     if (arr == null || arr.length == 0 || threadCount <= 0)
+    //         throw new IllegalArgumentException();
 
-    
+    //     threadCount = Math.min(threadCount, arr.length);
+    //     AtomicInteger minimalInteger = new AtomicInteger(Integer.MAX_VALUE);
+
+
+    // }
+
+    public static void TcpReceiver(int port) {
+        try (ServerSocket socket = new ServerSocket(port)){
+            System.out.println("Listening on port " + port);
+
+            Socket sock = socket.accept();
+            System.out.println("Client connected");
+
+            InputStream in = sock.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("Received" + line);
+            }
+            sock.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
